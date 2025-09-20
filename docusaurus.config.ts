945f6b8709898f8673cec2baf742a321f24f862a
DaJYu <1906681540@qq.com>
2025-09-20 22:49:00 +0800

    up navbar

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -31,7 +31,7 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-
+  // trailingSlash: false,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ttswap', // Usually your GitHub org/user name.
